3868e626fb03fdabf6db4da7e1e4d53449f5e2f9
Jon Ochoa <jonochoaestarrona@gmail.com>
2018-01-11 10:16:45 +0100

    Delete global.php

--- config/global.php
+++
@@ -1,9 +0,0 @@
-<?php
-define("CONTROLADOR_DEFECTO", "Bodega");
-define("ACCION_DEFECTO", "index");
-define('PATH', $_SERVER['DOCUMENT_ROOT']);
-
-define("TABLE_BODEGAS", "bodegas");
-define("TABLE_VINOS", "vinos");
-
-?>
