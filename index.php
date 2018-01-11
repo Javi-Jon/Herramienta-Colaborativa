4c59d7378adcd0c98a19bc782c6a09252c66af99
javier <javiermdg998@gmail.com>
2018-01-11 13:49:16 +0100

    estructura index

--- index.php
+++ index.php
@@ -4,7 +4,19 @@ session_start();
 if(!isset($_SESSION['idusuario'])){
     login();
 }elseif (isset ($_GET['controller'])) {
-    switch ($_GET['controller']){
-        case
-    }    
+   switch ($_GET['controller']) {
+    case'':
+        $controller = new VinoController();
+        action($controller);       
+        break;
+    default:
+        index();
+        break;       
+   }
+} else {
+index();    
 }
+function action($objController) {
+    $objController->run($_GET['action']);
+}
+
