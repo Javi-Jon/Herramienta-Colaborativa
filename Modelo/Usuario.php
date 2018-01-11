b26234944140d783ee0d4dbe7d1d948fb60f19d5
javier <javiermdg998@gmail.com>
2018-01-11 13:35:10 +0100

    acabar usuario NOBD

--- Modelo/Usuario.php
+++ Modelo/Usuario.php
@@ -7,4 +7,49 @@ class Usuario extends BD{
     private $password;
     private $tipo;
     private $fullname;
+    
+    function __construct() {
+        $this->tabla="usuarios";
+    }
+    function getId() {
+        return $this->id;
+    }
+
+    function getUsername() {
+        return $this->username;
+    }
+
+    function getPassword() {
+        return $this->password;
+    }
+
+    function getTipo() {
+        return $this->tipo;
+    }
+
+    function getFullname() {
+        return $this->fullname;
+    }
+
+    function setId($id) {
+        $this->id = $id;
+    }
+
+    function setUsername($username) {
+        $this->username = $username;
+    }
+
+    function setPassword($password) {
+        $this->password = $password;
+    }
+
+    function setTipo($tipo) {
+        $this->tipo = $tipo;
+    }
+
+    function setFullname($fullname) {
+        $this->fullname = $fullname;
+    }
+
+
 }
