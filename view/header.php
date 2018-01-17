49542836f6954c8a2ec372d922a368ce46c7d886
Jon <jonochoaestarrona@gmail.com>
2018-01-17 10:57:25 +0100

    Header y footer

---
+++ view/header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE HTML>
+<html lang="es">
+    <head>
+        <meta charset="utf-8"/>
+        <title>Gestión de bodegas</title>
+    </head>
+    <body>
+        <div class="container">
+            <div class="titulo">
+                <h1>Bodegas</h1>
+            </div>
